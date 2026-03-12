f2537957e089f8af5f30148e3f64ef40616c1c5c
Hümmət Əzimzadə <69711064+hummxt@users.noreply.github.com>
2026-03-13 05:55:58 +1100

    feat: google OAuth Connected

--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -1,11 +1,17 @@
-
 export function slugify(text: string): string {
-    return text
+    const azeToEng: Record<string, string> = {
+        'ə': 'e', 'ç': 'c', 'ö': 'o', 'ğ': 'g', 'ş': 's', 'ı': 'i', 'ü': 'u',
+        'Ə': 'e', 'Ç': 'c', 'Ö': 'o', 'Ğ': 'g', 'Ş': 's', 'I': 'i', 'Ü': 'u',
+    };
+
+    const transliterated = text.split('').map(char => azeToEng[char] || char).join('');
+
+    return transliterated
         .toString()
         .toLowerCase()
         .trim()
         .replace(/\s+/g, '-')           // Replace spaces with -
-        .replace(/[^\w-]+/g, '')         // Remove all non-word chars
+        .replace(/[^\w-]+/g, '')        // Remove all non-word chars
         .replace(/--+/g, '-')           // Replace multiple - with single -
         .replace(/^-+/, '')             // Trim - from start of text
         .replace(/-+$/, '');            // Trim - from end of text
