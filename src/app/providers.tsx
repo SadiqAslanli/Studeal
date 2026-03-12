31e87f8432bd7f8c6de2c38cf19ab21f781d4f90
Hümmət Əzimzadə <69711064+hummxt@users.noreply.github.com>
2026-03-13 04:44:04 +1100

    fix: Delay in auth solved

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,17 @@
 "use client";
 
-import { AuthProvider } from "@/context/AuthContext";
+import { AuthProvider, User } from "@/context/AuthContext";
 import { LanguageProvider } from "@/context/LanguageContext";
 
-export function Providers({ children }: { children: React.ReactNode }) {
+export function Providers({ 
+  children, 
+  initialUser 
+}: { 
+  children: React.ReactNode, 
+  initialUser?: User | null 
+}) {
   return (
-    <AuthProvider>
+    <AuthProvider initialUser={initialUser}>
       <LanguageProvider>
         {children}
       </LanguageProvider>
